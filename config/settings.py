d10ca09fc176cfd487471071c9cc859147cd031d
bishwajit93 <bish.karm123@gmail.com>
2025-08-06 00:35:35 +0200

    Use DATABASE_PUBLIC_URL instead of DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ.get("DATABASE_PUBLIC_URL"),
         conn_max_age=600,
         conn_health_checks=True,
     )
