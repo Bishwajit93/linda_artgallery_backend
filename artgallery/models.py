2f1894401d65b367f14e75fd818b8d353620ebc7
bishwajit93 <bish.karm123@gmail.com>
2025-08-06 00:31:23 +0200

    Setup HeroSection model and connected Railway PostgreSQL

--- artgallery/models.py
+++ artgallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class HeroSection(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    background_image = models.ImageField(upload_to='hero_images/', blank=True, null=True)
+    background_video = models.FileField(upload_to='hero_videos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title or "Untitled Hero Section"
