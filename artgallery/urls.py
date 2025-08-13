3397d336471a121fddec33f577fb00f6e6d97a85
bishwajit93 <bish.karm123@gmail.com>
2025-08-13 20:41:53 +0200

    Fix as_view typo in VideoDetailView URL

--- artgallery/urls.py
+++ artgallery/urls.py
@@ -3,5 +3,5 @@ from .views.video_views import VideoListView, VideoDetailView
 
 urlpatterns = [
     path("videos/", VideoListView.as_view(), name="video-list"),
-    path("videos/<int:id>/", VideoDetailView.as_View(), name="veideo-detail"),
+    path("videos/<int:id>/", VideoDetailView.as_view(), name="veideo-detail"),
 ]
