3d76164c0bebb5aaebbc342d2a5841b90e0d5f90
bishwajit93 <bish.karm123@gmail.com>
2025-09-19 12:33:05 +0200

    Fix HeroImage viewset

--- artgallery/views/heroimage_views.py
+++ artgallery/views/heroimage_views.py
@@ -1,14 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 from artgallery.models.heroimage import HeroImage
 from artgallery.serializers.heroimage_serializers import HeroImageSerializer
 
 class HeroImageViewSet(viewsets.ModelViewSet):
     serializer_class = HeroImageSerializer
+    queryset = HeroImage.objects.all().order_by("order", "-created_at")
     parser_classes = [MultiPartParser, FormParser]
 
-    def get_queryset(self):
-        qs = HeroImage.objects.all().order_by("order", "-created_at")
-        if not self.request.user.is_staff:
-            qs = qs.filter(is_active=True)
-        return qs
+    def create(self, request, *args, **kwargs):
+        # Ensure file is passed
+        if "image" not in request.FILES:
+            return Response({"error": "No image file provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
